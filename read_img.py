ca45d00f2a710971dc947cc0373e3938ccff195e
AndreF045 <andreferreiraa045@gmail.com>
2022-09-23 19:30:18 +0100

    openCV_tuto

---
+++ read_img.py
@@ -0,0 +1,15 @@
+#read image
+
+import cv2 as cv
+import sys
+img = cv.imread("/home/andre/Pictures/man.jpg")
+
+if img is None:
+    sys.exit("Could not read the image.")
+
+cv.imshow("este é o shek", img)
+k = cv.waitKey(0)
+
+if k == ord("s"):
+    cv.imwrite("man.png", img)
+
