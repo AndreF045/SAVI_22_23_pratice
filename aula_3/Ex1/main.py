a31b8400e0e79e87d4a1c58c2d9e9eededeb8d38
AndreF045 <andreferreiraa045@gmail.com>
2022-09-29 19:37:17 +0100

    traking

--- aula_3/Ex1/main.py
+++ aula_3/Ex1/main.py
@@ -1,11 +1,30 @@
+import cv2
+import numpy as np
 
 
 
 def main():
+    cap = cv2.VideoCapture('/home/andre/Videos/traffic.mp4')
 
-    print("check!")
-    print(5+55)
+    #deteçao de objetos da camara estatica
+    object_detector = cv2.createBackgroundSubtractorMOG2()
 
+    while True:
+
+        ret,frame = cap.read()
+
+        mask = object_detector.apply(frame)
+
+        cv2.imshow("frame", frame)
+        cv2.imshow("mask", mask)
+
+    
+        if cv2.waitKey(30) == 27:
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()
+    
 
 
 
